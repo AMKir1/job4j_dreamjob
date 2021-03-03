71b2775bed5d99687892f57a70cb747187b2d2cc
Andrei Kirillovykh <andykirill@gmail.com>
2021-03-03 19:22:39 +0300

    1. Написать тест на PostServlet.[2518#453795]

--- src/test/java/ru/job4j/dream/servlet/PostServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/PostServletTest.java
@@ -2,16 +2,15 @@ package ru.job4j.dream.servlet;
 
 import org.junit.Test;
 import ru.job4j.dream.PsqlStore;
-
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 public class PostServletTest {
@@ -25,4 +24,19 @@ public class PostServletTest {
         new PostServlet().doPost(req, resp);
         assertThat(PsqlStore.instOf().findPostById(1).getName(), is("Client"));
     }
+
+    @Test
+    public void whenGetPost() throws ServletException, IOException {
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        HttpSession session = mock(HttpSession.class);
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+        when(req.getSession()).thenReturn(session);
+        when(req.getRequestDispatcher("posts.jsp")).thenReturn(dispatcher);
+        new PostServlet().doGet(req, resp);
+        verify(req).getRequestDispatcher("posts.jsp");
+        verify(req, times(1)).getSession();
+        verify(dispatcher).forward(req, resp);
+    }
+
 }
